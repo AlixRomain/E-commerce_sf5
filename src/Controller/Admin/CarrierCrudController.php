689b123708ea54d1dfe3c9161df49bdf853fd5ea
AlixRomain <59125701+XAD66689@users.noreply.github.com>
2020-12-09 11:20:40 +0100

    Création des entité orders et ordersDetail

---
+++ src/Controller/Admin/CarrierCrudController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Carrier;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class CarrierCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Carrier::class;
+    }
+
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('name', 'Type de commande'),
+            TextEditorField::new('description', 'Descrition'),
+            MoneyField::new('price', 'Prix')->setCurrency('EUR'),
+        ];
+    }
+
+}
--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Carrier;
 use App\Entity\Category;
 use App\Entity\Product;
 use App\Entity\User;
@@ -33,6 +34,8 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkToCrud('Utilisateurs', 'fas fa-user', User::class);
         yield MenuItem::linkToCrud('Catégorie', 'fas fa-list', Category::class);
         yield MenuItem::linkToCrud('Produits', 'fas fa-tag', Product::class);
+        yield MenuItem::linkToCrud('Commandes', 'fas fa-truck', Carrier::class);
+
 
     }
 }
